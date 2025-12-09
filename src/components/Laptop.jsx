8f012ad5ebd70a18a95ecb1cd816fe43b014eff5
lean1835 <lean1835.vac@gmail.com>
2025-12-09 19:14:03 +0700

    anne

--- src/components/Laptop.jsx
+++ src/components/Laptop.jsx
@@ -3,23 +3,24 @@ import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 
 export default function Laptop(props) {
-  const { scene } = useGLTF("/models/laptop.glb");
-  const ref = useRef(); 
+  const { scene } = useGLTF("/models/laptop2.glb");
+  const ref = useRef();
 
+  // Xoay model mỗi frame
   useFrame((state, delta) => {
     if (ref.current) {
-      ref.current.rotation.x += delta * 0.5; 
-      ref.current.rotation.y += delta * 0.5; 
-      ref.current.rotation.z += delta * 0.5; 
+      ref.current.rotation.x += delta * 0.5;
+      ref.current.rotation.y += delta * 0.5;
+      ref.current.rotation.z += delta * 0.5;
     }
   });
 
   return (
     <primitive
-      ref={ref} 
+      ref={ref}
       object={scene}
       scale={0.09}
-      rotation={[0, Math.PI, 0]} 
+      rotation={[0, Math.PI, 0]}
       {...props}
     />
   );
